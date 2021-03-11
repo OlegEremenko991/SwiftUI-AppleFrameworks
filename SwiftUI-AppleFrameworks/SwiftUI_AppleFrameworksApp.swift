8e5a6ab3cd460764916acc1be96f5b8857ab0150
Oleg991 <o.n.eremenko@gmail.com>
2021-03-11 22:20:30 +0400

    Initial Commit

---
+++ SwiftUI-AppleFrameworks/SwiftUI_AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleFrameworksApp.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by Олег Еременко on 11.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
