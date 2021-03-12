f53cd8fa1814f8091299bbc165a4922ab37970f7
Oleg991 <o.n.eremenko@gmail.com>
2021-03-12 10:47:06 +0400

    Refactor + project restructured

--- SwiftUI-AppleFrameworks/Screens/FrameworkGridView/FrameworkGridView.swift
+++ SwiftUI-AppleFrameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -34,24 +34,6 @@ struct FrameworkGridView: View {
     }
 }
 
-struct FrameworkTitleView: View {
-    let framework: Framework
-
-    var body: some View {
-        VStack (spacing: 10) {
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.7)
-        }
-        .padding()
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
---
+++ SwiftUI-AppleFrameworks/Views/View/FrameworkTitleView.swift
@@ -0,0 +1,32 @@
+//
+//  FrameworkTitleView.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by Олег Еременко on 12.03.2021.
+//
+
+import SwiftUI
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+
+    var body: some View {
+        VStack (spacing: 10) {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.7)
+        }
+        .padding()
+    }
+}
+
+struct FrameworkTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkTitleView(framework: MockData.sampleFramework)
+    }
+}
